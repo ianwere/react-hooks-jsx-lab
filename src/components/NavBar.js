72d453c3c4f11ad73d76eb21b50b7ffe9929e28f
Ian Were <ian.were@student.moringaschool.com>
2023-10-26 17:35:12 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
-
+import Home from './Home';
+import About from './About';
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+
+  return (
+  
+  <nav>
+   <h1>NavBar</h1>
+  <a href = "#home">Home</a>
+  <a href = "#about">About</a>
+  </nav>
+ 
+  
+  )
 }
 
 export default NavBar;
